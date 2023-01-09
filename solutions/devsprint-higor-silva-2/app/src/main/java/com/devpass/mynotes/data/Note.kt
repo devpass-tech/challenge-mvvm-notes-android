d3b9fa20e91ef21a92249e463e0994c33b3731ce
Yan <yyan1207@gmail.com>
2023-01-09 16:47:54 -0300

    Create TimeStampConverter

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/data/Note.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/data/Note.kt
@@ -3,6 +3,8 @@ package com.devpass.mynotes.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.devpass.mynotes.utils.TimeStampConverter
 
 
 @Entity(tableName = "note_table")
@@ -11,7 +13,6 @@ data class Note(
     @ColumnInfo(name = "title") val title:String?,
     @ColumnInfo(name = "content") val content:String?,
     @ColumnInfo(name = "color") val color:String?,
-    @ColumnInfo(name = "time_stamp") val timestamp:String?,
+    @ColumnInfo(name = "time_stamp") @TypeConverters(TimeStampConverter::class) val timestamp:String?,
 
-
-)
+    )
