a6ebb8dcd3891de6440c19b9b2c43718e4659777
Yan <yyan1207@gmail.com>
2023-01-09 16:37:19 -0300

    Create Note entity

---
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/data/Note.kt
@@ -0,0 +1,17 @@
+package com.devpass.mynotes.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "note_table")
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id:Int?,
+    @ColumnInfo(name = "title") val title:String?,
+    @ColumnInfo(name = "content") val content:String?,
+    @ColumnInfo(name = "color") val color:String?,
+    @ColumnInfo(name = "time_stamp") val timestamp:String?,
+
+
+)
