2ef10c19f3b1757894c28ac2786830d880a77906
Yan <yyan1207@gmail.com>
2023-01-09 16:52:44 -0300

    Create NotesDao

---
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/data/NotesDao.kt
@@ -0,0 +1,33 @@
+package com.devpass.mynotes.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+
+@Dao
+interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from note_table order by title ASC")
+    fun getAllNotesOrderedByTitle(): LiveData<List<Note>>
+
+    @Query("Select * from note_table order by time_stamp ASC")
+    fun getAllNotesOrderedByDate(): LiveData<List<Note>>
+
+    @Query("Select * from note_table order by color ASC")
+    fun getAllNotesOrderedByColor(): LiveData<List<Note>>
+
+    @Update
+    suspend fun update(note: Note)
+
+}
