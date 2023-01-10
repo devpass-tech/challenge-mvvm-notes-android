dfb61c47d17caa2b0055acc06f919830a2bc5911
Yan <yyan1207@gmail.com>
2023-01-10 17:27:16 -0300

    refactor model

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/domain/model/Note.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/domain/model/Note.kt
@@ -3,8 +3,6 @@ package com.devpass.mynotes.domain.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
-import com.devpass.mynotes.utils.TimeStampConverter
 
 
 @Entity(tableName = "note_table")
@@ -12,7 +10,6 @@ data class Note(
     @PrimaryKey(autoGenerate = true) val id:Int?,
     @ColumnInfo(name = "title") val title:String?,
     @ColumnInfo(name = "content") val content:String?,
-    @ColumnInfo(name = "color") val color:String?,
-    @ColumnInfo(name = "time_stamp") @TypeConverters(TimeStampConverter::class) val timestamp:String?,
-
+    @ColumnInfo(name = "color") val color:Int,
+    @ColumnInfo(name = "time_stamp") val timeStamp:Long
     )
