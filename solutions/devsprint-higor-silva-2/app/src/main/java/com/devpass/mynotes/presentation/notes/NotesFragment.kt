197420fb2647685255cc4f7b3edd012df8b95da0
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-11 14:04:51 -0300

    feat: creating fun to show snackbar

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesFragment.kt
@@ -1,10 +1,20 @@
 package com.devpass.mynotes.presentation.notes
 
 import androidx.fragment.app.Fragment
+import com.devpass.mynotes.R
+import com.google.android.material.snackbar.Snackbar
 
 
 class NotesFragment : Fragment() {
 
+    private fun showUndoSnackBar() {
 
+        Snackbar.make(
+            requireView(),
+            R.string.message_note_deleted,
+            Snackbar.LENGTH_SHORT
+        ).setAction(R.string.message_undo_delete) {
 
+        }.show()
+    }
 }
--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -1,5 +1,12 @@
 package com.devpass.mynotes.presentation.notes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devpass.mynotes.domain.model.NoteModel
 
-class NotesViewModel : ViewModel()
+class NotesViewModel : ViewModel(){
+
+    var deletedNote = MutableLiveData<NoteModel>()
+
+}
