0f9dd78d99944c3cb3813616e64fdeec4fe93266
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-11 10:44:42 -0300

    feat: creating fun onNoteClicked

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesFragment.kt
@@ -1,10 +1,31 @@
 package com.devpass.mynotes.presentation.notes
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
+import com.devpass.mynotes.databinding.FragmentNotesBinding
+import com.devpass.mynotes.domain.model.NoteModel
 
 class NotesFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding =
+            FragmentNotesBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
 
+    private fun onNoteClicked(noteClicked: NoteModel){
+        val action =
+            NotesFragmentDirections.actionNotesFragmentToEditorFragment2()
+
+        findNavController().navigate(action)
+    }
 }
