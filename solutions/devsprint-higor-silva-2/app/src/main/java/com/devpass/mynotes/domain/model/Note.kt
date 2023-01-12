8b9002e6f6572fdbe6f1e6db32226bd16dcf8976
Yan <yyan1207@gmail.com>
2023-01-12 10:23:39 -0300

    Create companion object with a list of colors to model

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/domain/model/Note.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/domain/model/Note.kt
@@ -8,9 +8,15 @@ import com.devpass.mynotes.R
 
 @Entity(tableName = "note_table")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id:Int?,
-    @ColumnInfo(name = "title") val title:String,
-    @ColumnInfo(name = "content") val content:String,
-    @ColumnInfo(name = "color") val color:Int = R.color.yellow,
-    @ColumnInfo(name = "time_stamp") val timeStamp:Long
-    )
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "color") val color: Int = R.color.yellow,
+    @ColumnInfo(name = "time_stamp") val timeStamp: Long
+) {
+    companion object {
+        val noteColors = listOf(
+            R.color.yellow, R.color.purple, R.color.orange, R.color.blue, R.color.teal
+        )
+    }
+}
