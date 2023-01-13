88db736c7ca5978847c72aea64d45c5ae983210d
Yan <yyan1207@gmail.com>
2023-01-13 12:20:24 -0300

    Create color change when the colors buttons are clicked

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
@@ -1,5 +1,67 @@
 package com.devpass.mynotes.presentation.editor
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.devpass.mynotes.R
+import com.devpass.mynotes.databinding.FragmentEditorBinding
 
-class EditorFragment : Fragment()
+class EditorFragment : Fragment() {
+
+    private lateinit var binding: FragmentEditorBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEditorBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.purpleButton.setOnClickListener {
+            context?.let {
+                val color = ContextCompat.getColor(it, R.color.purple)
+                binding.noteLayout.setBackgroundColor(color)
+            }
+        }
+        binding.orangeButton.setOnClickListener {
+            context?.let {
+                val color = ContextCompat.getColor(it, R.color.orange)
+                binding.noteLayout.setBackgroundColor(color)
+            }
+        }
+        binding.blueButton.setOnClickListener {
+            context?.let {
+                val color = ContextCompat.getColor(it, R.color.blue)
+                binding.noteLayout.setBackgroundColor(color)
+            }
+        }
+        binding.greenButton.setOnClickListener {
+            context?.let {
+                val color = ContextCompat.getColor(it, R.color.teal)
+                binding.noteLayout.setBackgroundColor(color)
+            }
+        }
+        binding.yellowButton.setOnClickListener {
+            context?.let {
+                val color = ContextCompat.getColor(it, R.color.yellow)
+                binding.noteLayout.setBackgroundColor(color)
+            }
+        }
+
+        binding.confirmButton.setOnClickListener {
+            // save note
+        }
+    }
+}
