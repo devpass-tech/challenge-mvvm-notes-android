022cde434e4362e9aa63b83e0c2e530b6712f1ba
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-16 09:58:20 -0300

    chore: add fun deleteNote and undoDelete in NotesViewModel

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -3,10 +3,29 @@ package com.devpass.mynotes.presentation.notes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.devpass.mynotes.domain.model.Note
+import com.devpass.mynotes.domain.usecase.DeleteNoteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NotesViewModel : ViewModel(){
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val deleteNoteUseCase: DeleteNoteUseCase
+) : ViewModel(){
 
-    var deletedNote = MutableLiveData<Note>()
+    var deletedNote = MutableLiveData<Note?>()
 
+    fun deleteNote(note: Note){
+        deletedNote.value = note
+
+        viewModelScope.launch {
+            deleteNoteUseCase(note)
+        }
+    }
+
+    fun undoDelete(){
+        deletedNote.value = null
+    }
 }
