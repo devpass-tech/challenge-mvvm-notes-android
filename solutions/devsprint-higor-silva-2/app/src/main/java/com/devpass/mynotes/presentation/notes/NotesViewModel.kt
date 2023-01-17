ef616308ae1eccb531c6bf2ac0b5eb98cfe73679
Yan <yyan1207@gmail.com>
2023-01-17 14:59:29 -0300

    Allow viewModel to add Note

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -3,10 +3,38 @@ package com.devpass.mynotes.presentation.notes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.devpass.mynotes.domain.exceptions.InvalidNoteException
 import com.devpass.mynotes.domain.model.Note
+import com.devpass.mynotes.domain.usecase.AddNoteUseCase
+import com.devpass.mynotes.domain.usecase.NoteUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class NotesViewModel : ViewModel(){
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases
+) : ViewModel(){
 
     var deletedNote = MutableLiveData<Note>()
+    private val _snackBar = MutableLiveData<String?>()
+    val snackbar: LiveData<String?>
+        get() = _snackBar
+
+
+    fun insertNote(note: Note){
+        viewModelScope.launch {
+            try {
+                noteUseCases.addNote.invoke(note)
+            } catch (e: InvalidNoteException){
+                _snackBar.value = e.message
+            }
+        }
+    }
+
+    fun onSnackBarShown(){
+        _snackBar.value = null
+    }
 
 }
