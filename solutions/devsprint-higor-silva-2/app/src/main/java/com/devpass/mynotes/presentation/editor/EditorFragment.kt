40f72e7c407f8c023266c49cd85e491f4f34fb85
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-17 10:03:15 -0300

    feature: fill EditorFragment with args.noteClicked

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import com.devpass.mynotes.R
 import com.devpass.mynotes.databinding.FragmentEditorBinding
 
-//Esperando Yan concluir tarefa Create editor view
-//para fazer checagem de nulabilidade e assim preencher a tela
 class EditorFragment : Fragment(){
 
     private val args: EditorFragmentArgs by navArgs()
     
     private lateinit var binding: FragmentEditorBinding
+
+    private lateinit var titleEditText: EditText
+    private lateinit var contentEditText: EditText
+    private lateinit var noteLayout: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +28,18 @@ class EditorFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentEditorBinding.inflate(inflater, container, false)
+
+        args.noteClicked?.let {
+            val noteClicked = it
+
+            titleEditText = binding.titleEditText
+            contentEditText = binding.contentEditText
+            noteLayout = binding.noteLayout
+
+            titleEditText.setText(noteClicked.title)
+            contentEditText.setText(noteClicked.content)
+            noteLayout.setBackgroundColor(resources.getColor(noteClicked.color))
+        }
 
         return binding.root
     }
