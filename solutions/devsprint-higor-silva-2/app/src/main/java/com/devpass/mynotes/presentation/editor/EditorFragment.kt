55243b2ed019aa459d1c4e358c79ede72b6f4a18
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-18 11:31:10 -0300

    fix: adjunting EditorFragment Layout

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -20,7 +20,7 @@ class EditorFragment : Fragment(){
 
     private lateinit var titleEditText: EditText
     private lateinit var contentEditText: EditText
-    private lateinit var noteLayout: LinearLayout
+    private lateinit var noteLayout: ConstraintLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.devpass.mynotes.R
 import com.devpass.mynotes.domain.model.Note
 import com.devpass.mynotes.domain.usecase.NotesManagerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,6 +12,8 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import java.time.Instant
+import java.util.*
 
 @HiltViewModel
 class NotesViewModel @Inject constructor(
@@ -27,11 +30,11 @@ class NotesViewModel @Inject constructor(
 
     private fun insertNote() = viewModelScope.launch {
         val note = Note(
-            id = 123,
-            title = "Nova nota",
-            content = "klsdkaslkdlkas",
-            color = 0,
-            timeStamp = 0,
+            id = 46,
+            title = "Add your title",
+            content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            color = R.color.yellow,
+            timeStamp = Date.from(Instant.now()).time,
         )
         manager.add(note = note)
     }
@@ -40,6 +43,4 @@ class NotesViewModel @Inject constructor(
         currentList.value = notes
 
     }.launchIn(viewModelScope)
-
-
 }
