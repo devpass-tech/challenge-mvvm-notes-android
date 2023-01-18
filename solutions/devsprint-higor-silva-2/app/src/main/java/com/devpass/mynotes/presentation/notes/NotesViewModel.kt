812860705da9509aa03e43754b7b0fb6ab0f8b2e
Yan <yyan1207@gmail.com>
2023-01-18 15:55:48 -0300

    fix conflicts

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.devpass.mynotes.R
 import com.devpass.mynotes.domain.exceptions.InvalidNoteException
 import com.devpass.mynotes.domain.model.Note
 import com.devpass.mynotes.domain.usecase.NotesManagerUseCase
@@ -13,8 +12,6 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import java.time.Instant
-import java.util.*
 
 @HiltViewModel
 class NotesViewModel @Inject constructor(
@@ -25,8 +22,6 @@ class NotesViewModel @Inject constructor(
 
     private val currentList = MutableLiveData<List<Note>>()
     fun observeCurrentList(): LiveData<List<Note>> = currentList
-
-    var deletedNote = MutableLiveData<Note>()
     private val _snackBar = MutableLiveData<String?>()
     val snackbar: LiveData<String?>
         get() = _snackBar
