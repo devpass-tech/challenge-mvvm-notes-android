9c0404b0497bf186712e484c005b43a5cc782038
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-01-18 16:58:07 -0300

    fix: adjusting imports

--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/editor/EditorFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
--- solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
+++ solutions/devsprint-higor-silva-2/app/src/main/java/com/devpass/mynotes/presentation/notes/NotesViewModel.kt
@@ -4,15 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.devpass.mynotes.R
+import com.devpass.mynotes.domain.exceptions.InvalidNoteException
 import com.devpass.mynotes.domain.model.Note
 import com.devpass.mynotes.domain.usecase.NotesManagerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
-import java.time.Instant
-import java.util.*
+import javax.inject.Inject
 
 @HiltViewModel
 class NotesViewModel @Inject constructor(
